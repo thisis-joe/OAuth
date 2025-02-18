75997deb35015a78acabea721f3de9513c4eb9cb
joseph <wdg0434@gmail.com>
2025-02-18 10:49:29 +0900

    feat: Change URL to access comment controller

--- src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/comments")
+@RequestMapping("/api/v1/{postId}/comments")
 public class ApiV1CommentController {
 
 }
